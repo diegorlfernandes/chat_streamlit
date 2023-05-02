2cfb6711d39c6a7976b464da6884abc291eb000f
Diego Fernandes <diegorlfernandes@hotmail.com>
2023-05-02 16:54:10 -0400

    inicial

---
+++ ChatGPT.py
@@ -0,0 +1,40 @@
+import os
+import openai
+from ChatGPTContext import ChatGPTContext as ct
+class ChatGPT():
+    
+    def __init__(self,api_key):
+        openai.api_key = api_key
+        self.context = ct.get_context_pizzaria()
+
+    def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature, 
+        )
+        return response.choices[0].message["content"]
+
+    def collect_messages(self,msg):
+        prompt = msg
+        self.context.append({'role':'user', 'content':f"{prompt}"})
+        response = self.get_completion_from_messages(self.context) 
+        self.context.append({'role':'assistant', 'content':f"{response}"})
+        return f'Atendente: {response}\n'
+
+
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature, 
+        )
+        return response.choices[0].message["content"]
+    
+if __name__ == '__main__':
+    
+    openai.api_key = "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"
+
+    get_completion_from_messages("Bom dia!")
+    
---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from streamlit_chat import message
+import requests
+from ChatGPT import ChatGPT
+
+st.set_page_config(
+    page_title="Streamlit Chat - Demo",
+    page_icon=":robot:"
+)
+
+st.header("Pizzaria")
+    
+container = st.container()
+
+user_input = st.text_input("You: ", key="input")
+
+if 'user' not in st.session_state:
+    st.session_state['user'] = []
+
+if 'bot' not in st.session_state:
+    st.session_state['bot'] = []
+
+ 
+if user_input:
+    GPT = ChatGPT(st.secrets['api_key'])
+
+    response = GPT.collect_messages(user_input)
+    
+    st.session_state['user'].append("Usuário: "+user_input) 
+    st.session_state['bot'].append("Bot: "+response)
+
+    if st.session_state['user']:
+        with container:
+            for i in range(len(st.session_state['user'])):
+                message(st.session_state["user"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["bot"][i], key=str(i)+'_bot')
